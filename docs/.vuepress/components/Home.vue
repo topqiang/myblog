7e46cafe84d182c5fe96fe772370f839b4dbfa4d
topqiang <topqiang@kanziqiang.top>
2018-12-22 12:25:25 +0800

    首次提交

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <p align="center">
+            <a href="https://gitee.com/topqiang" target="_blank">
+                <img class="myheader" width="150" src="myheader.png" alt="logo">
+            </a>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+
+
+}
+</script>
+
+<style>
+    .myheader{
+        border-radius: 50%;
+        border: 4px solid #ffdddd;
+        animation: .5s ease-in-out;
+        transform: skew(0,0);
+    }
+    .myheader:hover{
+        animation-name: myheader;
+    }
+    @keyframes myheader{
+        10%{
+            transform: skew(10deg,15deg);
+        }
+        30%{
+            transform: skew(10deg,5deg);
+        }
+        50%{
+            transform: skew(10deg,-5deg);
+        }
+        70%{
+            transform: skew(10deg,-15deg);
+        }
+        90%{
+            transform: skew(0deg,-15deg);
+        }
+        100%{
+            transform: skew(0,0);
+        }
+    }
+</style>
+
---
+++ docs/README.md
@@ -0,0 +1,19 @@
+<Home/>
+
+``` js
+export default {
+  getMyProfession () {
+    let str = "!redoc SJ a m'I"
+    return str.split("").reverse().join("")
+  }
+}
+// "I'm a JS coder!"
+```
+
+::: tip
+个人信条：
+热爱生活，热爱工作，坚持大前端路线不动摇！
+:::
+
+-----
+:ghost:[个人简历](/self-info/):clap:[项目经历](/project/) :punch:[知识积累](/knowledge/):100:
